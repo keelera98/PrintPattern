1cc846c96066b3d479aa0af8b96d3bcb3c9931d8
keelera98 <32946655+keelera98@users.noreply.github.com>
2017-10-20 23:04:44 -0400

    Test 1
    Testing the pattern, not sure yet.

---
+++ pattern.java
@@ -0,0 +1,16 @@
+public class pattern{
+	public static void main(String args[]){
+		int i, j;
+		
+		i = 99
+		while(i > 0){
+			j = 0;
+			while(j < 11){
+				System.out.printf("i: %d j: %d\n", i, j);
+				i--;
+				j++;
+			}
+			i = i - 18;
+		}
+	}
+}
